f3b6103605b58f9fb637c34f133dee647b0b7c77
1MioSubs <it09022022@gmail.com>
2023-02-10 16:21:49 +0200

    10.02.2023 commit - пробовал сделать прослушивание без остановки

--- main.py
+++ main.py
@@ -7,19 +7,18 @@ sr.pause_threshold = 0.5
 
 commands_dict = {
     'commands': {
-        'greeting': ['привет', 'приветствую'],
-        'create_task': ['добавить задачу', 'создать задачу', 'заметка'],
-        'play_music': ['включить музыку', 'дискотека']
+        'greeting': ['привет'],
+        'create_task': ['добавить задачу', 'создать заметку'],
+        'play_music': ['музыку'],
+        'stop_bot': ['стоп']
     }
 }
 
 
-def listen_command():
-    """The function will return the recognized command"""
-    
+def listen_command():    
     try:
         with speech_recognition.Microphone() as mic:
-            sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+            #sr.adjust_for_ambient_noise(source=mic, duration=0.5)
             audio = sr.listen(source=mic)
             query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
             
@@ -28,17 +27,16 @@ def listen_command():
         return 'Damn... Не понял что ты сказал :/'
 
 
-def greeting():
-    """Greeting function"""
-    
-    return 'Привет нищеброд!'
 
 
-def create_task():
-    """Create a todo task"""
-    
-    print('Что добавим в список дел?')
-    
+def greeting():    
+    return 'Привет мой повелитель =) мяу'
+
+
+
+
+def create_task():    
+    print('Есть какие планы?')
     query = listen_command()
         
     with open('todo-list.txt', 'a') as file:
@@ -47,14 +45,30 @@ def create_task():
     return f'Задача {query} добавлена в todo-list!'
 
 
-def play_music():
-    """Play a random mp3 file"""
-    
+
+def play_music():    
     files = os.listdir('music')
     #random_file = f'music/{random.choice(files)}'
     os.system(f'xdg-open music/test.mp3')
-    
+
     return f'Танцуем под {random_file.split("/")[-1]} 🔊🔊🔊'
+
+
+
+def stop_bot():
+    i = 0
+
+
+
+i = 1
+
+while i == 1:
+    query = listen_command()
+    
+    for k, v in commands_dict['commands'].items():
+        if query in v:
+            print(globals()[k]())
+
 
 
 def main():
@@ -65,5 +79,5 @@ def main():
             print(globals()[k]())
         
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
